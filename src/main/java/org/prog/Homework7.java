155d73a180c082e86111a0b5544a3c064f849374
maruk1996 <marukov96@gmail.com>
2025-06-11 16:31:24 +0200

    Homework 7, v.7,1

--- src/main/java/org/prog/Homework7.java
+++ src/main/java/org/prog/Homework7.java
@@ -7,15 +7,30 @@ public class Homework7 {
     public static void main(String[] args) {
         System.out.println(UUID.randomUUID());
 
-        Random random = new Random(50);
+        Random rand = new Random();
+
+        String[] cities = new String[5];
+        cities[0] = "Warsaw";
+        cities[1] = "Paris";
+        cities[2] = "Madrid";
+        cities[3] = "Amsterdam";
+        cities[4] = "London";
+
+
         Plane2[] planes = new Plane2[10];
         for (int i = 0; i < planes.length; i++) {
-            UUID flightId = UUID.randomUUID();
-            int maxPassengers = random.nextInt(100, 200);
-            int currentPassengers = random.nextInt(maxPassengers + 1);
-            int available_seats = maxPassengers - currentPassengers;
-            planes[i] = new Plane2(flightId, maxPassengers, currentPassengers, available_seats);
+            planes[i] = new Plane2();
+            planes[i].flightId = UUID.randomUUID();
+            planes[i].destinationCity = cities[rand.nextInt(cities.length)];
+            planes[i].maxPassengers = rand.nextInt(100, 200);
+            planes[i].currentPassengers = rand.nextInt(planes[i].maxPassengers + 1);
+            planes[i].availableSeats = planes[i].maxPassengers - planes[i].currentPassengers;
+
+
+
+
             planes[i].goTo();
+
         }
 
         System.out.println("Planes with half or less seats occupied:");
--- src/main/java/org/prog/Plane2.java
+++ src/main/java/org/prog/Plane2.java
@@ -5,30 +5,18 @@ import java.util.UUID;
 
 public class Plane2 {
 
-    public int flightId;
+    public UUID flightId;
     public int maxPassengers;
     public int currentPassengers;
-    public int available_seats;
+    public int availableSeats;
     public String destinationCity;
 
 
-
-
-//    public Plane2(UUID flightId, int maxPassengers, int currentPassengers, int availableSeats) {
-        this.flightId = new Random().nextInt(1000, 9999);
-        this.maxPassengers = maxPassengers;
-        this.currentPassengers = currentPassengers;
-        this.available_seats = availableSeats;
-        String[] cities = {"Warsaw", "London", "Roma", "Amsterdam", "New York", "Madrid", "Paris", "Barcelona"};
-        int randomIndex = new Random().nextInt(cities.length);
-        this.destinationCity = cities[randomIndex];
-
-
-}
-
     public void goTo() {
+
+
         System.out.println("The plane " + flightId +
                 " is going to " + destinationCity + " with " + currentPassengers + " passengers out of " +
-                maxPassengers + " possible. " + available_seats + " available seats");
+                maxPassengers + " possible. " + availableSeats + " available seats");
     }
-    }
+}
